c6109b8696690f75f5f16f66b280e0bfab3a1f97
Alan Evans <evansa@bur5-imac49039.local>
2014-08-26 13:26:21 +0100

    My_v2 Jousting.

--- BrainFuck/tests/com/coltsoftware/brainfuck/BrainFuckJoustTests.java
+++ BrainFuck/tests/com/coltsoftware/brainfuck/BrainFuckJoustTests.java
@@ -19,12 +19,12 @@ public class BrainFuckJoustTests {
 	@Before
 	public void setup() {
 		setupGame(30);
-		pointer = new StandardTapePointer(tape);
-		pointerreversed = ReverseTapePointer.reverse(tape);
 	}
 
 	private void setupGame(int tapeLength) {
 		tape = new Tape(tapeLength);
+		pointer = new StandardTapePointer(tape);
+		pointerreversed = ReverseTapePointer.reverse(tape);
 		TapePointer reg = new StandardTapePointer(tape);
 		TapePointer rev = ReverseTapePointer.reverse(tape);
 		new BrainFuck(reg, setTo128).execute();
@@ -59,17 +59,86 @@ public class BrainFuckJoustTests {
 		System.out.print(tape.toString());
 	}
 
+	private final static String ALTERNATOR = "(>+>-)*4>+(>[-][.])*21";
+	private final static String DECOYBOT = ">(+)*10>(-)*10(>+>-)*3(>[-].)*21";
+	private final static String COUNTERPUNCH = "+>(-)*12(>)*7(<(-)*12<(+)*12)*3(>)*7(([-([(-)*6[+].])*5])*4>)*21";
+
+	private final static String MY_V2 = "(>-)*9(+[-]>)*20";
+
 	@Test
-	public void pit() {
-		String program1 = "(>+>-)*4>+(>[-][.])*21";
-		String program2 = "(>)*19(>[-])*11";
+	public void pit_against_alternator() {
+		String program1 = ALTERNATOR;
+		// String program2 = "(>)*19(>[-][.])*11";
+		String program2 = "(>)*9(+[-]>)*20";
+		int score = pitPrograms(program1, program2);
+		assertEquals(1, score);
+		System.out.print("End");
+	}
+
+	@Test
+	public void version_2_pit_against_alternator() {
+		String program1 = ALTERNATOR;
+		String program2 = MY_V2;
+		int score = pitPrograms(program1, program2);
+		assertEquals(1, score);
+		System.out.print("End");
+	}
+
+	@Test
+	public void version_2_pit_against_self() {
+		String program2 = MY_V2;
+		int score = pitPrograms(program2, program2);
+		assertEquals(0, score);
+		System.out.print("End");
+	}
+
+	@Test
+	public void version_2_pit_against_decoybot() {
+		String program1 = DECOYBOT;
+		String program2 = MY_V2;
+		int score = pitPrograms(program1, program2);
+		assertEquals(1, score);
+		System.out.print("End");
+	}
+	
+	@Test
+	public void version_2_pit_against_counter_punch() {
+		String program1 = COUNTERPUNCH;
+		String program2 = MY_V2;
+		int score = pitPrograms(program1, program2);
+		assertEquals(1, score);
+		System.out.print("End");
+	}
+
+	private int pitPrograms(String program1, String program2) {
+		int score = 0;
+		for (int i = 10; i <= 30; i++) {
+			setupGame(i);
+			score += joust(program1, program2);
+			System.out.print(tape.toString());
+			System.out.print("\n");
+		}
+		return score;
+	}
+
+	private int joust(String program1, String program2) {
 		BrainFuck engine1 = new BrainFuck(pointer, program1);
 		BrainFuck engine2 = new BrainFuck(pointerreversed, program2);
 		int zeroCount1 = 0;
 		int zeroCount2 = 0;
 		for (int i = 0; i < 10000; i++) {
-			engine1.executeSingleStep();
-			engine2.executeSingleStep();
+			try {
+				engine1.executeSingleStep();
+			} catch (Exception ex) {
+				System.out.print("Prog 1 end of tape:\n");
+				return -1;
+			}
+			try {
+				engine2.executeSingleStep();
+			} catch (Exception ex) {
+				System.out.print("Prog 2 end of tape:\n");
+				return 1;
+			}
 			if (tape.getAt(0) == 0)
 				zeroCount1++;
 			else
@@ -78,16 +147,21 @@ public class BrainFuckJoustTests {
 				zeroCount2++;
 			else
 				zeroCount2 = 0;
+			if (zeroCount1 == 2 && zeroCount2 == 2) {
+				System.out.print("Draw by death:\n");
+				return 0;
+			}
 			if (zeroCount1 == 2) {
-				System.out.print("Player 1 dead");
-				break;
+				System.out.print("Prog 2 clasic win:\n");
+				return -1;
 			}
 			if (zeroCount2 == 2) {
-				System.out.print("Player 2 dead");
-				break;
+				System.out.print("Prog 1 clasic win:\n");
+				return 1;
 			}
 		}
-		System.out.print("End");
+		System.out.print("Draw:\n");
+		return 0;
 	}
 
 }
