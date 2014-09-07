2d79b204fbf005a4ae4833da0c8ae4e34758f238
Alan Evans <thealanevans@googlemail.com>
2014-09-07 13:50:25 +0100

    20 populations concurrently developed. Seed 1700 Generation 400 Score: 90.24 % 1137/122/1 1505820/529075/10000 RawCode: +++>-----+----+---------+-+------+---->-+------+-+------+---->----------->>------<------>-+------+---->-----------------<-+--+-----<------>-+-------<->>-------------<------>---->---------<------>-+-------<->>-----------[>-[+][++-+[]]-]>------
    TightCode:
    +++>------------------------>-------------->----------->>------<------>--------->-----------------<------<------>-------<->>-------------<------>---->---------<------>-------<->>-----------[>-[+][++[]]-]>------

--- BrainFuckGenetics/src/com/coltsoftware/brainfuckgenetics/Main.java
+++ BrainFuckGenetics/src/com/coltsoftware/brainfuckgenetics/Main.java
@@ -24,7 +24,7 @@ public final class Main {
 					getGenerationNumber(args[0]));
 			genetics.go();
 		} else {
-			List<Genetics> populations = createPopulations(bots, 10);
+			List<Genetics> populations = createPopulations(bots, 20);
 			int size = populations.size();
 			while (true) {
 				for (int i = 0; i < size; i++) {
